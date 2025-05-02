e90534825a51ac660234ca64a9386651f7b28847
Manoj Maharjan <manoj.maharjan@hotmail.com>
2025-05-02 11:41:00 +0200

    install package joi and add category

--- db/associations.js
+++ db/associations.js
@@ -1,5 +1,11 @@
 import sequelize from "./index.js";
+import Category from "../models/Category.js";
+import Product from "../models/Product.js";
 
-// table/model relations can be defined here
+Category.hasMany(Product, {
+  foreignKey: "categoryId",
+  as: "products",
+  onDelete: "CASCADE", //deletes products when category is deleted
+});
 
 sequelize.sync();
