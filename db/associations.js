7c0a437d789ea263379804564d6acf585bafc156
LukasF7889 <lukas_fritsch@hotmail.de>
2025-05-02 15:44:19 +0200

    Added Order Schema, Controller, Router

--- db/associations.js
+++ db/associations.js
@@ -1,11 +1,25 @@
 import sequelize from "./index.js";
 import Category from "../models/Category.js";
 import Product from "../models/Product.js";
+import Order from "../models/Order.js";
+import User from "../models/User.js";
 
 Category.hasMany(Product, {
   foreignKey: "categoryId",
   as: "products",
   onDelete: "CASCADE", //deletes products when category is deleted
+});
+
+Order.belongsTo(User, {
+  foreignKey: "userId",
+});
+
+User.belongsToMany(Product, {
+  through: "OrderProducts",
+});
+
+Product.belongsToMany(Order, {
+  through: "OrderProducts",
 });
 
 sequelize.sync();
